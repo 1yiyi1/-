3760b632f1b42856c59b71b8457666d98359bc65
1yiyi1 <88219531+1yiyi1@users.noreply.github.com>
2021-07-31 10:59:10 +0800

    Add files via upload

---
+++ get_features.py
@@ -0,0 +1,142 @@
+#coding=utf-8
+import numpy as np
+import pandas as pd
+import os
+import glob 
+'''
+def Corr_coeff(y_true,y_predict):
+	y_mean = np.mean(y_true)
+	SSR = 0
+	SST = 0
+	#print(len(y_true))
+	#print(len(y_predict))
+	for i in range(len(y_true)):
+		SSR = SSR + (y_predict[i]-y_mean)**2
+		SST = SST + (y_true[i]-y_mean)**2
+	return float(SSR)/float(SST)
+'''	
+def draw_data(times_list,volt_list):
+	time_list_part = []
+	volt_list_part = []
+	for i in range(len(times_list)):
+		if times_list[i] >500.0 and times_list[i] < 1500.0:
+			time_list_part.append(times_list[i])
+			volt_list_part.append(volt_list[i])
+	return time_list_part,volt_list_part
+	
+
+def Get_Feature_discharge(dict_discharge,dots_list):
+
+
+	times_list = list(dict_discharge['Time'])
+	volt_list = list(dict_discharge['Voltage_measured'])
+	temp_list = list(dict_discharge['Temperature_measured'])
+	cap = list(dict_discharge['Capacity'])[0]
+	##########################
+	time_list_part,volt_list_part = draw_data(times_list,volt_list)
+	fix_ = np.polyfit(time_list_part,volt_list_part,4)
+	fix_equation = np.poly1d(fix_)
+	volt_predict_corr = fix_equation(time_list_part)
+	volt_predict = fix_equation(dots_list)
+	############################
+	
+	volt_min = min(volt_list)
+	volt_index_min = volt_list.index(volt_min)
+	a = (volt_list[0]-volt_min)
+	b = float(times_list[volt_index_min]-times_list[0])/float(3600)
+	teq_vd = float(b)/float(a)
+	veq_td = float(a)/float(b)
+	
+	
+	
+	#corr = Corr_coeff(volt_list_part,volt_predict_corr)
+	MVF = sum(4.2-np.array(volt_predict))/float(len(volt_predict))
+	Tst = temp_list[0]
+	Tavg = np.mean(temp_list)
+	Vavg = np.mean(volt_list)
+	
+	
+	
+	return cap,MVF,Tst,Tavg,Vavg,teq_vd,veq_td
+	
+def Get_Feature_charge(df_charge):
+	times_list = list(df_charge['Time'])
+	volt_list = list(df_charge['Voltage_measured'])
+	current_list = list(df_charge['Current_measured'])
+	
+	for ii in range(len(volt_list)):
+		if volt_list[ii] >=4.2:
+			key_index = ii
+			break
+	for jj in range(len(volt_list)-1,-1,-1):
+		if current_list[jj] >=0.02:
+			key_index_down = jj
+			break
+	thv = float(times_list[key_index_down]-times_list[key_index])/float(3600)
+	
+	time_up_volt = float(times_list[key_index]-times_list[0])/float(3600)
+	up_volt = volt_list[key_index]-volt_list[0]
+	#print(volt_list[key_index])
+	#print(up_volt)
+	teq_vc = time_up_volt/up_volt
+	veq_vc = up_volt/time_up_volt
+
+	c = current_list[key_index]-current_list[key_index_down]
+	d = float(times_list[key_index_down]-times_list[key_index])/float(3600)
+	leq_tc = c/d
+	
+	return thv,teq_vc,veq_vc,leq_tc
+	
+
+
+os.chdir(os.getcwd())
+lines = '{}	'*12+'\n'
+feature_txt = open(str(os.getcwd().split('\\')[-1])+'_feature.txt','w+')
+feature_txt.write(lines.format('cycle','cap','MVF','corr','Tst','Tavg','Vavg','teq_vd','veq_td','thv','teq_vc','veq_vc','leq_tc'))
+xlsd_files_discharge = glob.glob('discharge_*.xls')
+xlsd_files_discharge = sorted(xlsd_files_discharge,key = lambda x:int(x.split('_')[1][:-4]))
+
+xlsd_files_charge = glob.glob('charge_*.xls')
+xlsd_files_charge = sorted(xlsd_files_charge,key = lambda x:int(x.split('_')[1][:-4]))
+
+xlsd_files_charge.pop(32)
+xlsd_files_charge.pop(-1)
+#print(xlsd_files_charge)
+
+dots_list = [i for i in range(500,1500,10)]
+dict_feature = {}
+
+
+
+to_df = [[] for _ in range(11)]
+
+for discharge_file,charge_file in zip(xlsd_files_discharge,xlsd_files_charge):
+	df_discharge = pd.read_excel(discharge_file)
+	df_charge = pd.read_excel(charge_file)
+	cap,MVF,Tst,Tavg,Vavg,teq_vd,veq_td = Get_Feature_discharge(df_discharge,dots_list)
+	thv,teq_vc,veq_vc,leq_tc = Get_Feature_charge(df_charge)
+	feature_list = [cap,MVF,Tst,Tavg,Vavg,teq_vd,veq_td,thv,teq_vc,veq_vc,leq_tc]
+	
+	for n,to_df_cell in enumerate(to_df):
+		to_df_cell.append(feature_list[n])
+	
+	#feature_txt.write(str(discharge_file.split('_')[1][:-4])+'	'+str(cap)+'	'+str(MVF)+'	'+str(corr)+'	'+str(Tst)+'	'+str(Tavg)+
+	#'	'+str(Vavg)+'	'+str(teq_vd)+'	'+str(veq_td)+'	'+str(thv)+'\n')
+	
+	#feature_txt.write(lines.format(str(discharge_file.split('_')[1][:-4]),str(cap),str(MVF),str(corr),str(Tst),str(Tavg),str(Vavg),str(teq_vd),str(veq_td),str(thv)
+	#,str(teq_vc),str(veq_vc),str(leq_tc)))
+	feature_txt.write(lines.format(str(discharge_file.split('_')[1][:-4]),*feature_list))
+#print(np.array(feature_list))
+df_feature = pd.DataFrame(np.array(to_df).T,columns=['capacity','MVF','Tst','Tavg','Vavg','teq_vd','veq_td','thv','teq_vc','veq_vc','leq_tc'])
+df_feature.to_excel('dataset.xls')	
+feature_txt.close()
+#
+#
+#
+#
+#
+#
+#
+
+#mvf = Get_MVF(df,dots_list)
+#print(mvf)
---
+++ nn_capacity_R1.py
@@ -0,0 +1,156 @@
+#coding=utf-8
+import numpy as np
+import pandas as pd
+import os
+import glob
+import torch
+
+os.chdir(os.getcwd())
+
+xls = 'dataset.xls'
+df =pd.read_excel(xls)
+batch_size = 3
+#print(df.loc[0].values)
+
+train_dataset = torch.tensor(df.loc[:99].values)   ###train set
+
+test_dataset = torch.tensor(df.loc[100:].values)   ###test set
+
+
+train_feature = train_dataset[:,2:].float() ## get features
+train_label = train_dataset[:,1].float()  ## target
+test_feature = test_dataset[:,2:].float()
+test_label = test_dataset[:,1].float()
+
+import torch.utils.data as Data
+
+dataset = Data.TensorDataset(train_feature,train_label)
+data_iter = Data.DataLoader(dataset,batch_size,shuffle=True)
+
+dataset_test = Data.TensorDataset(test_feature,test_label)
+data_test_iter = Data.DataLoader(dataset_test,batch_size,shuffle=True)
+
+#continue
+from torch import nn
+
+D_in = 10  ##cell of input layer
+H1 = 9 ## cell of hidden layer
+H2=9
+H3=9
+D_out = 1 ## cell of output layer
+lerning_rate = 1e-4   
+#class MyModel(nn.Moduel):
+#	def __init__(self,D_in,H,D_out):
+#		super(MyModel,self).__init__()
+#		self.linear1 = nn.Linear(D_in,H)
+#		self.linear2 = nn.Linear(H,D_out)
+#	def forward(x,self):
+#		h_relu = nn.ReLU(self.linear1(x))
+#		y_pred = self.linear2(h_relu)
+#		return y_pred
+	
+		
+class MyModel(nn.Module):
+	def __init__(self,D_in,H1,H2,H3,D_out):
+		super(MyModel,self).__init__()
+		self.net = nn.Sequential(nn.Linear(D_in,H1),nn.ReLU(),nn.Linear(H1,H2),nn.ReLU(),nn.Linear(H2,H3),nn.ReLU(),nn.Linear(H3,D_out))   ## You can increase the hidden layer and change the activation function		                                                                              		                                                                           
+	def forward(self,x):
+		y_pred = self.net(x)
+		return y_pred
+		
+model = MyModel(D_in,H1,H2,H3,D_out)
+
+optimizer = torch.optim.Adam(model.parameters(),lr=lerning_rate)  ## Adam 
+loss_fn = nn.MSELoss(reduction='mean') 	
+
+epoch = 2000  ## epoch of training, you can change it
+txt = open('result.txt','w+')
+train_loss_list = []
+test_loss_list = []
+epoch_list = []
+for ii in range(epoch):
+	train_loss,n = 0,0
+	epoch_list.append(ii)
+	for x,y in data_iter:
+		#print(x.shape)
+		y = y.view(-1,1)
+		
+		#print(y.shape)
+		y_pred = model(x)
+		#print(y_pred.shape)
+		loss = loss_fn(y_pred,y)
+		optimizer.zero_grad()
+		loss.backward()  
+		optimizer.step()
+		train_loss = train_loss + loss.item()
+		n = n + 1
+	#break
+	test_loss,m = 0,0
+	for x_test,y_test in data_test_iter:
+		y_test = y_test.view(-1,1)
+		y_test_pred = model(x_test)
+		l = loss_fn(y_test_pred,y_test)
+		test_loss = test_loss + l.item()
+		m = m + 1
+	train_loss = train_loss/float(n)
+	test_loss = test_loss/float(m)
+	train_loss_list.append(train_loss)
+	test_loss_list.append(test_loss)
+	txt.write('epoch: {},train loss:{},test loss:{}\n'.format(str(ii),train_loss,test_loss))
+	
+	
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(epoch_list,train_loss_list,linestyle='-.',marker='o',label='train')
+plt.plot(epoch_list,train_loss_list,linestyle='-.',marker='>',label='test')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+
+plt.legend()
+#plt.show()
+plt.savefig("result_train.png",dpi=800, bbox_inches='tight')
+plt.clf()	
+txt_result = open('result_nn.txt','w+')
+#y_true_list = []
+#y_prediction_list = []
+
+
+cycle_num = []
+true_list = []
+prediction_list = []
+for cc in range(len(test_feature)):
+	
+	y_prediction = model(test_feature[cc,:])
+	txt_result.write(str(cc+1)+'	'+str(test_label[cc].item())+'	'+str(y_prediction.data.item())+'\n')
+	cycle_num.append(cc+1)
+	true_list.append(test_label[cc].item())
+	prediction_list.append(y_prediction.data.item())
+plt.figure()
+plt.plot(cycle_num,true_list,linestyle='-',label='True')
+plt.plot(cycle_num,prediction_list,linestyle='-',label='Prediction')
+plt.xlabel('Circles')
+plt.ylabel('capacity')
+plt.legend()
+#plt.show()
+plt.savefig("result_prediction.png",dpi=800, bbox_inches='tight')
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
